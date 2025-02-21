cac0df63eb8c5ad9e9c2c9c38d7104b9a4e6aff1
Dax Fernandez-Abascal Fernandez <daxferna@c1r12s6.42madrid.com>
2025-02-21 19:43:50 +0100

    all done

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,7 @@
 /*   By: daxferna <daxferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 22:12:41 by daxferna          #+#    #+#             */
-/*   Updated: 2025/02/19 21:02:19 by daxferna         ###   ########.fr       */
+/*   Updated: 2025/02/21 19:42:57 by daxferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,23 @@ void	error(int errno, t_map *game)
 	if (errno == 1)
 	{
 		free_game(game);
-		ft_putstr_fd("No se pudo inicializar la ventana\n", 2);
+		ft_putstr_fd("Couldn't initialize window\n", 2);
 	}
 	if (errno == 2)
-		ft_putstr_fd("Número incorrecto de argumentos\n", 2);
+		ft_putstr_fd("Incorrect number of arguments\n", 2);
 	else if (errno == 3)
-		ft_putstr_fd("El argumento no es un archivo válido (.ber)\n", 2);
+		ft_putstr_fd("The argument is not a valid file (.ber)\n", 2);
 	else if (errno == 4)
-		ft_putstr_fd("No contiene los caracteres necesarios\n", 2);
+		ft_putstr_fd("The map doesn't have the correct number of elements\n", 2);
 	else if (errno == 5)
-		ft_putstr_fd("El mapa es demasiado pequeño\n", 2);
+		ft_putstr_fd("The map is too small\n", 2);
 	else if (errno == 6)
-		ft_putstr_fd("El mapa no es rectangular\n", 2);
+		ft_putstr_fd("The map is not rectangular\n", 2);
 	else if (errno == 7)
-		ft_putstr_fd("El mapa no está cerrado\n", 2);
+		ft_putstr_fd("The map is not closed\n", 2);
 	else if (errno == 8)
-		ft_putstr_fd("El mapa contiene caracteres inválidos\n", 2);
+		ft_putstr_fd("The map has invalid characters\n", 2);
 	else if (errno == 9)
-		ft_putstr_fd("El mapa no es solucionable\n", 2);
+		ft_putstr_fd("The map is not solvable\n", 2);
 	exit(1);
 }
